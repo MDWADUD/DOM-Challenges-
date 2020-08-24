fb7eb718812d71901f9f5d0475240692fad61fd0
Md Abdullah All Wadud <43837530+MDWADUD@users.noreply.github.com>
2020-08-24 05:05:31 -0400

    DOM Challenge number 1

---
+++ DOM1/javaScript.js
@@ -0,0 +1,40 @@
+//Write the code necessary to do the following:
+//1) Select the section with an id of container without using querySelector.
+let sec = document.getElementById("container");
+
+//2) Select the section with an id of container using querySelector.
+let x = document.querySelector("#container");
+
+//3) Select all of the list items with a class of "second".
+
+let y = document.getElementsByClassName('second');
+//4) Select a list item with a class of third, but only the list item inside of the ol tag.
+
+let n = document.querySelector('ol.third');
+//5) Give the section with an id of container the text "Hello!".
+
+let text = container.innerText = "Hello!";
+//6) Add the class main to the div with a class of footer.
+
+document.querySelector('div.footer').classList.add('main');
+//7) Remove the class main on the div with a class of footer.
+
+document.querySelector('div.footer').classList.remove('main');
+//8) Create a new li element.
+
+let new = document.createElement("li");
+//9) Give the li the text "four".
+
+let new.innerText = "four"; 
+//10) Append the li to the ul element.
+
+document.querySelector("ul").appendChild(new)
+//11) Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+let list = document.querySelectorAll("ol li")
+for (let i=0; i<list.length; i++){
+    list[1].getElementsByClassName.backGroundColor ="green"
+}
+//12) Remove the div with a class of footer.
+let footer = document.querySelector(".footer")
+footer.remove();
